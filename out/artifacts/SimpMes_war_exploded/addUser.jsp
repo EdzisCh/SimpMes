405eaae4d9b7946f1526b4274ea1bf73c4aa6f16
EdzisCh <edzis95@mail.ru>
2018-09-26 21:39:32 +0300

    Second commit

---
+++ out/artifacts/SimpMes_war_exploded/addUser.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Привет
+  Date: 26.09.2018
+  Time: 19:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
--- src/main/java/app/servlets/AddUserServlet.java
+++ src/main/java/app/servlets/AddUserServlet.java
@@ -1,6 +1,7 @@
 package app.servlets;
 
 import java.io.IOException;
+import javax.servlet.RequestDispatcher;
 
 /**
  * Created by Привет on 26.09.2018.
@@ -16,6 +17,7 @@ public class AddUserServlet extends javax.servlet.http.HttpServlet {
   protected void doGet(javax.servlet.http.HttpServletRequest request,
       javax.servlet.http.HttpServletResponse response)
       throws javax.servlet.ServletException, IOException {
-
+    RequestDispatcher requestDispatcher = request.getRequestDispatcher("views/addUser.jsp");
+    requestDispatcher.forward(request, response);
   }
 }
