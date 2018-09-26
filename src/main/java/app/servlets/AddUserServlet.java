819344ad6ea68fa1572d6281a7e0ef95f6c8e0a4
EdzisCh <edzis95@mail.ru>
2018-09-26 21:03:06 +0300

    Initial commit

---
+++ src/main/java/app/servlets/AddUserServlet.java
@@ -0,0 +1,21 @@
+package app.servlets;
+
+import java.io.IOException;
+
+/**
+ * Created by Привет on 26.09.2018.
+ */
+public class AddUserServlet extends javax.servlet.http.HttpServlet {
+
+  protected void doPost(javax.servlet.http.HttpServletRequest request,
+      javax.servlet.http.HttpServletResponse response)
+      throws javax.servlet.ServletException, IOException {
+
+  }
+
+  protected void doGet(javax.servlet.http.HttpServletRequest request,
+      javax.servlet.http.HttpServletResponse response)
+      throws javax.servlet.ServletException, IOException {
+
+  }
+}
